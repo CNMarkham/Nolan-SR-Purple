fb09846a93fe0c58cda604cc9e5059af05470e62
CNMarkham <markhamonca@codeninjas.com>
2023-06-15 17:46:13 -0400

    breakout finished

--- Breakout/Assets/Ploinko.cs
+++ Breakout/Assets/Ploinko.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Ploinko : MonoBehaviour
 {
+    public TMP_Text scoreText;
+    private int score;
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = 0;
     }
 
     // Update is called once per frame
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("block"))
-    {
-        Destroy(collision.gameObject);
+        {
+            Destroy(collision.gameObject);
+            score++;
+            scoreText.text = score.ToString();
+        }
     }
 }
+
