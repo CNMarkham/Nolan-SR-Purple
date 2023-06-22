98d0a2a3a18115cac09e4439815f5000d25e370a
Miles <markhamonca@codeninjas.com>
2023-06-22 18:51:32 -0400

    step49 space

--- Space Invaders/Assets/Enemy.cs
+++ Space Invaders/Assets/Enemy.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public Animator animator;
     public float speed;
+    private Vector2 direction;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        direction = Vector2.right;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        animator.SetTrigger("death");
+        Destroy(gameObject, 1f);
+        Destroy(collision.gameObject);
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Translate(Vector2.left * Time.deltaTime * speed);
-        }
+            transform.Translate(direction * Time.deltaTime * speed);
 
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (transform.position.x > -8f)
         {
-            transform.Translate(Vector2.right * Time.deltaTime * speed);
-        }s
+            direction = Vector2.left;
+        }
+        if (transform.position.x < -8f)
+        {
+            direction = Vector2.right;
+        }
     }
 }
     
