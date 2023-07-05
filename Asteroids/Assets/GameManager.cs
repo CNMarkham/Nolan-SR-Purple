98019e2e44d534541db5bb05bd053ffd7c1a5ce3
Elisa <132782158+AJsFakeGithubAccount@users.noreply.github.com>
2023-07-05 17:46:44 -0400

    space rock step 63

--- Asteroids/Assets/GameManager.cs
+++ Asteroids/Assets/GameManager.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript3 : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
+    public GameObject rockPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        for (int i = 0; i < 4; i++)
+        {
+            Vector3 randomPosition = new Vector3(Random.Range(-8, 8), Random.Range(-8, 8), 0f);
+            Instantiate(rockPrefab, randomPosition, Quaternion.identity);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
+
+
