d43d7d43ef877f8dfd3d4ae8800b512e57c57b01
CNMarkham <markhamonca@codeninjas.com>
2023-07-19 17:56:05 -0400

    Rac-Ran sterp 68

---
+++ Activity 8 Pac-Man (1980)/Assets/PacMan.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacMan : Movement
+{ 
+    protected override void ChildUpdate()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
