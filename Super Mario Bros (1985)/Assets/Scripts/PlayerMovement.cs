0220ff7b641fc7e26e62fe82716f19f12f8455f0
ccCNMarkham <132835392+CNMarkham@users.noreply.github.com>
2023-08-24 18:51:47 -0400

    marion step 70

--- Super Mario Bros (1985)/Assets/Scripts/PlayerMovement.cs
+++ Super Mario Bros (1985)/Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float moveSpeed;
+
+    private RaycastHit2D hit;
+    public float jumpForce;
+    private bool jumping;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        jumping = false;
     }
 
     // Update is called once per frame
@@ -18,5 +23,33 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
 
         rb.AddForce(Vector2.right * horizontal * moveSpeed * Time.deltaTime);
+
+        Jump();
+        FlipDirection();
+    }
+
+
+    private void Jump()
+    {
+        hit = Physics2D.CircleCast(rb.position, 0.25f, Vector2.down, 0.375f, LayerMask.GetMask("Default"));
+
+        if (hit.collider != null && Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 velocity = rb.velocity;
+            velocity.y = jumpForce;
+            rb.velocity = velocity;
+            jumping = true;
+            Invoke("ResetJumping", 0.5f);
+
+
+        }
+
+    }
+    private void FlipDirection()
+    {
+        foreach (SpriteRenderer sprite in GetComponentsInChildren<SpriteRenderer>())
+        {
+            sprite.flipX = rb.velocity.x < 0;
+        }
     }
 }
