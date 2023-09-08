4602eb0b6f17547cc68de6413908bce1fb385ae9
YO MOM <96213580+ilovetapwaterr@users.noreply.github.com>
2023-09-08 17:49:41 -0400

    marion step 155

--- Super Mario Bros (1985)/Assets/Scripts/PlayerMovement.cs
+++ Super Mario Bros (1985)/Assets/Scripts/PlayerMovement.cs
@@ -66,4 +66,34 @@ public class PlayerMovement : MonoBehaviour
             animator.SetBool("inAir", hit.collider == null || jumping);
         }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        float distance = 0.375f;
+
+        if (GetComponent<PlayerBehavior>().big)
+        {
+            distance += 1f;
+            RaycastHit2D hitTop = Physics2D.CircleCast(rb.position, 0.25f, Vector2.up, distance, LayerMask.GetMask("Default"));
+            if (hitTop.collider != null)
+            {
+                Debug.Log(hitTop.collider);
+                Vector3 velocity = rb.velocity;
+                velocity.y = 0;
+                rb.velocity = velocity;
+                jumping = false;
+
+
+                BlockHit blockHit = hitTop.collider.gameObject.GetComponent<BlockHit>();
+
+                if (blockHit != null)
+                {
+                    blockHit.Hit();
+                    Debug.Log("BlockHit2");
+                }
+            }
+
+            
+        }
+    }
 }
