6a31e283c60492dba6fcfe2dbac2e66cb5cbba78
Miles <markhamonca@codeninjas.com>
2023-09-20 17:44:44 -0400

    tetris step 11

--- Activity 10 Tetris (1984)/Assets/Scripts/Tetromino.cs
+++ Activity 10 Tetris (1984)/Assets/Scripts/Tetromino.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class Tetromino : MonoBehaviour
 {
+    public float speed = 25;
+    private float previousTime;
+    public float fallTime = 0.8f;
+    public float fallSpeed = 25;
 
 
     // Update is called once per frame
     void Update()
     {
-        //if ()
-        //{
 
-        //}
-        //else
-        //{
+        // Input.GetKeyDown
+        // Input.GetKeyUp
+        // Input.GetKey
 
-        //}
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        float tempTime = fallTime;
+        if ((Time.time - previousTime) > fallTime)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * fallSpeed);
+            previousTime = Time.time;
+        }
+
     }
 }
