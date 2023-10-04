953dd38863d7615a85bdf42d35de05da80be7ee8
rehehehe <markhamonca@codeninjas.com>
2023-10-04 17:56:09 -0400

    tetris step 38

---
+++ Activity 10 Tetris (1984)/Assets/Scripts/Spawner.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] tetrominoes;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+}
