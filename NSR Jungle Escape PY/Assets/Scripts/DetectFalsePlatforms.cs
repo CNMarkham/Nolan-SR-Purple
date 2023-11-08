bd5068fe9e1b803fe4c6a4a4d4b0d89adb323419
Roosham <markhamonca@codeninjas.com>
2023-11-08 17:57:24 -0500

    done

--- NSR Jungle Escape PY/Assets/Scripts/DetectFalsePlatforms.cs
+++ NSR Jungle Escape PY/Assets/Scripts/DetectFalsePlatforms.cs
@@ -7,6 +7,16 @@ public class DetectFalsePlatforms : MonoBehaviour
 
     void Update()
     {
+        bool hit = Physics.Raycast(transform.position, transform.forward, 3, 1 << 8);
+
+        if (hit)
+        {
+            Debug.LogWarning("Be Careful");
+        }
+        if (hit == false)
+        {
+            Debug.Log("All Clear");
+        }
 
     }
-}
+}                   
